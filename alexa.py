1a6dd14d291aa57df3387c3fc30c1d7c1980f52b
Dboss-Coder <106919036+Dboss-Coder@users.noreply.github.com>
2022-06-05 15:37:44 +0100

    added alexa

---
+++ alexa.py
@@ -0,0 +1,43 @@
+###p13 0001
+
+##defined variables
+trigger = 0
+active = 0
+
+##functions of use
+def activate(statment):
+    global active
+    acceptstatments = ["hey p1", "p1", "p13", "pie", "robobot"]
+    for i in range(0, len(acceptstatments)):
+        if statment == acceptstatments[i]:
+            active = 1
+            print("boop beep")
+        else:
+            continue
+      
+def request(statment):
+    if active == 1:
+        if statment == "hello":
+            print("bweep boo")
+        if statment == "calculate something":
+            symbol = input("symbol?  ")
+            x = int(input("x"))
+            y = int(input("y"))
+            if symbol in ["+", "-", "*", "/"]:
+                if symbol == "+":
+                    print(x + y)
+                if symbol == "-":
+                    print(x - y)
+                if symbol == "*":
+                    print(x * y)
+                if symbol == "/":
+                    print(x / y)
+            else:
+                print("?")
+  
+
+
+##implimentation of functions in if statments and for loops
+#activate p1
+activate("hey p1")
+request("calculate something")
